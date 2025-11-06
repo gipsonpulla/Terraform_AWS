69c04ed732fb9c1b3cfcfbca56f7278cd45b107b
Gipson Pulla <gipsonpulla@Gipsons-MacBook-Air.local>
2025-11-06 23:04:07 +0530

    adding the changes for basic ec2

---
+++ basic_ec2/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0157af9aea2eef346"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0484ed14577ad95f8"
+  tags = {
+    Name = "my-web-server"
+  }
+}

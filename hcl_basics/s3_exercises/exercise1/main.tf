d62d20899ab3f97eae568a7da25e177d768779f2
Gipson Pulla <pullagipson@gmail.com>
2026-04-02 19:29:42 +0530

    adding the steps for s3 buckets

---
+++ hcl_basics/s3_exercises/exercise1/main.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "marvel-cinematic-universe" {
+  bucket = "oppal123486kmp"
+
+}
---
+++ hcl_basics/s3_exercises/exercise1/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.39.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = var.region
+  s3_use_path_style = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+
+  #endpoints {
+  #  s3                       = "http://aws:4566"
+  #}
+}
+

9c57258c67018364fb4d143f58a8360af5dd7230
Gipson Pulla <pullagipson@gmail.com>
2026-04-09 17:15:15 +0530

    adding the snippet for backend s3

---
+++ hcl_basics/backend_s3/main.tf
@@ -0,0 +1,15 @@
+terraform {
+    backend "s3" {
+        bucket = "gips-tf-backend-state0912092321"
+        key = "terraform.tfstate"
+        use_lockfile = true 
+        region = "us-east-1"
+      
+    }
+}
+resource "local_file" "fruits" {
+    filename = "/tmp/fruits_list.txt"
+    content = "I love banana"
+    file_permission = "0700"
+  
+}

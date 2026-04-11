e43801e8da09acddc4ae969d8bae6917367e9ad0
Gipson Pulla <pullagipson@gmail.com>
2026-04-12 00:00:02 +0530

    adding the import snippet

--- hcl_basics/tf_import/main.tf
+++ hcl_basics/tf_import/main.tf
@@ -10,3 +10,11 @@ resource "aws_instance" "ruby" {
 output "instances" {
   value = aws_instance.ruby
 }
+
+#aws ec2 create-key-pair --key-name jade 
+#--query 'KeyMaterial' --output text 
+#> /root/terraform-projects/project-jade/jade.pem.
+
+#gipsonpulla:~/Terraform_AWS/hcl_basics$ aws ec2 describe-instances  | jq -r '.Reservations[].Instances[].InstanceId'
+#i-0c1f5c60dfgfgdf6df8gdfg920ddfg440
+#i-05sdfsdfdrfd134e5b39fe982aadertres
